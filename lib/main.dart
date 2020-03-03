25e741a264cb091e203ef7b0693fcbbdce5f6ce6
italolopes9 <it-alo19@hotmail.com>
2020-03-02 23:04:58 -0300

    Adicionado texto Login

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:app_guia/Animation/FadeAnimation.dart';
 
-void main() => runApp(
-  MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: HomePage(),
-  )
-);
+void main() => runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ));
 
 class HomePage extends StatelessWidget {
   @override
@@ -14,14 +13,41 @@ class HomePage extends StatelessWidget {
       body: Container(
         width: double.infinity,
         decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            colors: [
-              Colors.orange[900],
-              Colors.orange[800],
-              Colors.orange[400]
-            ]
-          )
+            gradient: LinearGradient(begin: Alignment.topCenter, colors: [
+          Colors.orange[900],
+          Colors.orange[800],
+          Colors.orange[400]
+        ])),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              height: 80,
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  FadeAnimation(
+                      1,
+                      Text(
+                        "Login",
+                        style: TextStyle(color: Colors.white, fontSize: 40),
+                      )),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  FadeAnimation(
+                      1.3,
+                      Text(
+                        "Bem - vindo",
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      )),
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
