0a27b5d561e31c3f35812a465605877ee8681809
italolopes9 <it-alo19@hotmail.com>
2020-03-02 23:52:51 -0300

    0

--- lib/main.dart
+++ lib/main.dart
@@ -47,12 +47,62 @@ class HomePage extends StatelessWidget {
                 ],
               ),
             ),
+            SizedBox(height: 20,),
             Expanded(
               child: Container(
                 decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(60), topRight: Radius.circular(60))
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(60), topRight: Radius.circular(60))
+                    ),
+              child: Padding(
+                padding: EdgeInsets.all(30),
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(height: 60,),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [BoxShadow(
+                          color: Color.fromRGBO(255, 95, 27, .3),
+                          blurRadius: 20,
+                          offset: Offset(0, 10)
+                        )]
+                      ),
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              border: Border(bottom: BorderSide(color: Colors.grey[200]))
+                            ),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: "Email",
+                                hintStyle: TextStyle(color: Colors.grey),
+                                border: InputBorder.none
+                              ),
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              border: Border(bottom: BorderSide(color: Colors.grey[200]))
+                            ),
+                            child: TextField(
+                              decoration: InputDecoration(
+                                hintText: "Senha",
+                                hintStyle: TextStyle(color: Colors.grey),
+                                border: InputBorder.none
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
                 ),
+              ),
               ),
             )
           ],
